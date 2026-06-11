8b7d280e58c653e845c400061ee27ccafb583843
Zixuan Zhang <zzxhotmail@gmail.com>
2026-06-11 18:34:27 +0800

    Add files via upload

---
+++ gold_pipeline/DimUser.py
@@ -0,0 +1,26 @@
+import dlt
+
+expectations = {
+    "rule_1" :"user_id IS NOT NULL"
+}
+
+
+@dlt.table              #标记下方函数为一张 DLT Live Table。
+@dlt.expect_all_or_drop(expectations)
+def dimuser_stag():
+    df = spark.readStream.table("spotify_cata.silver.dimuser")      #直接读取 Databricks 内已存在的表
+    return df           #把读取到的数据流返回，DLT 自动将其落地为 dimuser_stag 这张 DLT 表。
+
+
+dlt.create_streaming_table(name="dimuser") #提前定义一张空的流式 Delta 表 dimuser，作为 CDC/SCD 的最终目标表；
+
+dlt.create_auto_cdc_flow(
+    target="dimuser",               #要写入的目标表名
+    source = "dimuser_stag",        #CDC 数据来源
+    keys= ["user_id"],              #主键 用来判断 “同一条记录”
+    sequence_by= "updated_at",       #排序字段：解决乱序、判断谁是最新
+    stored_as_scd_type= 2,          # SCD2
+    track_history_except_column_list= None,     #不追踪哪些列， None = 所有字段变化都生成新版本
+    name= None,                     #给这个 CDC flow 起个名字， None = 默认用 target 名字（即 dimuser）
+    once= False                     #False = 持续流式跑（实时 / 准实时同步）
+)
